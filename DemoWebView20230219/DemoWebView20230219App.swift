7aec2082fb9c3ba86a515b21e84725d9621a10db
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-19 22:26:18 -0500

    Initial Commit

---
+++ DemoWebView20230219/DemoWebView20230219App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoWebView20230219App.swift
+//  DemoWebView20230219
+//
+//  Created by Xavier on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoWebView20230219App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
